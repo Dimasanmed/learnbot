cd95986f3d99c23a761d85fd1f69ea85f5745adf
Dimasanmed <malofeevdmitry@mail.ru>
2023-04-04 23:04:36 +0300

    /planet

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 import settings
+import ephem
+from datetime import date
 
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
@@ -9,6 +11,15 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
                     filename='bot.log',
                     encoding='utf-8'
                     )
+
+def planet(update, context):
+        planet_name = str(update.message.text.split(" ")[1]).lower().capitalize()
+        day = date.today()
+        planet = getattr(ephem, planet_name)(day)
+        const = ephem.constellation(planet)
+        answer = f"На сегодняшнию дату {day} планета {planet_name} находится в созвездии {const}"
+        update.message.reply_text(answer)
+
 
 def greet_user(update, context):
         text = 'Вызван /start'
@@ -29,7 +40,9 @@ def main():
 
         dp = mybot.dispatcher
         dp.add_handler(CommandHandler('start', greet_user))
+        dp.add_handler(CommandHandler("planet", planet))
         dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+
 
         mybot.start_polling()#ходить в telegram проверять сообщения
         mybot.idle()#работать бесконечно
