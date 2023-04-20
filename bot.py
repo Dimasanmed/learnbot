8f8e23a490a9a6cf6ffd3355ec649f34ccf36885
Dimasanmed <malofeevdmitry@mail.ru>
2023-04-20 22:25:00 +0300

    Добавил котиков и эмодзи

--- bot.py
+++ bot.py
@@ -1,9 +1,14 @@
 # Импортируем нужные компоненты
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from glob import glob
 import logging
-import settings
-import ephem
+from random import choice
+
+from emoji import emojize
 from datetime import date
+import ephem
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+import settings
 
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
@@ -11,6 +16,13 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
                     filename='bot.log',
                     encoding='utf-8'
                     )
+
+
+def greet_user(update, context):
+        emo = get_user_emo(context.user_data)
+        context.user_data['emo'] = emo
+        text = 'Привет {}'.format(emo)
+        update.message.reply_text(text)
 
 def planet(update, context):
         planet_name = str(update.message.text.split(" ")[1]).lower().capitalize()
@@ -20,27 +32,35 @@ def planet(update, context):
         answer = f"На сегодняшнию дату {day} планета {planet_name} находится в созвездии {const}"
         update.message.reply_text(answer)
 
-
-def greet_user(update, context):
-        text = 'Вызван /start'
-        logging.info(text)
-        update.message.reply_text(text)
-
 def talk_to_me(update, context):
-        user_text = "Привет {}! Ты написал: {}".format(update.message.chat.first_name, update.message.text)
+        emo = get_user_emo(context.user_data)
+        user_text = "Привет {} {}! Ты написал: {}".format(update.message.chat.first_name, emo, update.message.text)
         logging.info("User: %s,Chat id: %s, Message: %s", update.message.chat.first_name,
                      update.message.chat.id, update.message.text)
         update.message.reply_text(user_text)
 
+def send_cat_picture(update, context):
+        cat_list = glob('images/cat*.jp*g')
+        cat_pic = choice(cat_list)
+        context.bot.send_photo(chat_id=update.message.chat.id, photo=open(cat_pic, 'rb'))
+
+def get_user_emo(user_data):
+        if 'emo' in user_data:
+                return user_data['emo']
+        else:
+                user_data['emo'] = emojize(choice(settings.USER_EMOJI), language='alias')
+                return user_data['emo']
 
 def main():
-        mybot = Updater(settings.API_KEY,)
+        mybot = Updater(settings.API_KEY, use_context=True)
 
         logging.info('Бот запускается')
 
         dp = mybot.dispatcher
         dp.add_handler(CommandHandler('start', greet_user))
         dp.add_handler(CommandHandler("planet", planet))
+        dp.add_handler(CommandHandler('cat', send_cat_picture))
+
         dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
 
